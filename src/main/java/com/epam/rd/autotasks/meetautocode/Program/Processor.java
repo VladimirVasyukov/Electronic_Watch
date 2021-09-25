47bb733188f8344c131361c0cba749d2b3c4a622
Vladimir Vasyukov <vladimir.vasyukov7@gmail.com>
2021-09-25 19:21:58 +0300

    Fixing problems in class Processor

--- src/main/java/com/epam/rd/autotasks/meetautocode/Program/Processor.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/Program/Processor.java
@@ -2,13 +2,15 @@ package com.epam.rd.autotasks.meetautocode.Program;
 
 public class Processor {
     public int calculateHours(int seconds, int secondsPerMinute, int hoursPerDay) {
+        final int MINUTES_PER_HOUR = 60;
         int minutesTimesHours = seconds / secondsPerMinute;
-        return (minutesTimesHours / secondsPerMinute) % hoursPerDay;
+        return (minutesTimesHours / MINUTES_PER_HOUR) % hoursPerDay;
     }
 
     public int calculateMinutes(int seconds, int secondsPerMinute) {
+        final int MINUTES_PER_HOUR = 60;
         int minutesTimesHours = seconds / secondsPerMinute;
-        return minutesTimesHours % secondsPerMinute;
+        return minutesTimesHours % MINUTES_PER_HOUR;
     }
 
     public int calculateSeconds (int seconds, int secondsPerMinute) {
