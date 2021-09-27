71cc4dbca59476cac4478fb68e327bbd47380fb6
Vladimir Vasyukov <vladimir.vasyukov7@gmail.com>
2021-09-27 19:04:45 +0300

    Fixing problems in class Processor V 2.0

--- src/main/java/com/epam/rd/autotasks/meetautocode/Program/Controller.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/Program/Controller.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Controller {
     private final static int SECONDS_PER_MINUTE = 60;
+    private final static int MINUTES_PER_HOUR = 60;
     private final static int HOURS_PER_DAY = 24;
     private final Processor processor;
     private final View view;
@@ -18,8 +19,8 @@ public class Controller {
     public void calculate() {
         int seconds = scanner.nextInt();
         seconds = Math.max(seconds, 0);
-        view.printResult(processor.calculateHours(seconds, SECONDS_PER_MINUTE, HOURS_PER_DAY),
-            processor.calculateMinutes(seconds, SECONDS_PER_MINUTE),
+        view.printResult(processor.calculateHours(seconds, SECONDS_PER_MINUTE, MINUTES_PER_HOUR, HOURS_PER_DAY),
+            processor.calculateMinutes(seconds, SECONDS_PER_MINUTE, MINUTES_PER_HOUR),
             processor.calculateSeconds(seconds, SECONDS_PER_MINUTE));
     }
 }
--- src/main/java/com/epam/rd/autotasks/meetautocode/Program/Processor.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/Program/Processor.java
@@ -1,16 +1,14 @@
 package com.epam.rd.autotasks.meetautocode.Program;
 
 public class Processor {
-    public int calculateHours(int seconds, int secondsPerMinute, int hoursPerDay) {
-        final int MINUTES_PER_HOUR = 60;
+    public int calculateHours(int seconds, int secondsPerMinute, int minutesPerHour, int hoursPerDay) {
         int minutesTimesHours = seconds / secondsPerMinute;
-        return (minutesTimesHours / MINUTES_PER_HOUR) % hoursPerDay;
+        return (minutesTimesHours / minutesPerHour) % hoursPerDay;
     }
 
-    public int calculateMinutes(int seconds, int secondsPerMinute) {
-        final int MINUTES_PER_HOUR = 60;
+    public int calculateMinutes(int seconds, int secondsPerMinute, int minutesPerHour) {
         int minutesTimesHours = seconds / secondsPerMinute;
-        return minutesTimesHours % MINUTES_PER_HOUR;
+        return minutesTimesHours % minutesPerHour;
     }
 
     public int calculateSeconds (int seconds, int secondsPerMinute) {
